51161017227d4ebd2b930bc85cfd0e7a17c698de
Arda Sarı <77012292+ardasarico@users.noreply.github.com>
2025-03-21 00:20:09 +0300

    add: App home page

---
+++ src/components/app/AnimatedBackground.jsx
@@ -0,0 +1,12 @@
+const AnimatedBackground = () => {
+  return (
+    <iframe
+      className={
+        'pointer-events-none absolute mt-96 h-full w-full transition-opacity duration-1000 select-none'
+      }
+      src='https://unicorn.studio/embed/IWM5jP9QU8qwjalEMmdK'
+    />
+  );
+};
+
+export default AnimatedBackground;
